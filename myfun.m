46c6348b89b2c950fc918af011cac4c3c8d4f8fb
rolo5555 <61249373+rolo5555@users.noreply.github.com>
2020-03-06 18:35:41 -0300

    Delete myfun.m

--- myfun.m
+++
@@ -1,4 +0,0 @@
-function F = myfun(teta,teta2,teta1)
-F(1)= -2000*cos(teta1)+2000*cos(teta(1))-1280*cos(teta(2))+350*cos(teta2);
-F(2)= -2000*sin(teta1)+2000*sin(teta(1))-1280*sin(teta(2))+350*sin(teta2);
-end
